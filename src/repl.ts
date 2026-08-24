2e121876c073c9fe7a3a1a2f4b39e0b147f387c7
SpencerMcFadden <mcfadden.spencer@protonmail.com>
2026-08-24 11:52:01 -0400

    adds startREPL function. uses in main. fixes bad imports. adds config types.

--- src/repl.ts
+++ src/repl.ts
@@ -1,7 +1,29 @@
+import { createInterface } from 'node:readline';
+import { stdin, stdout } from 'node:process';
+
 export function cleanInput(input:string): string[]{
   return input
     .toLowerCase()
     .trim()
     .split(" ")
     .filter((word) => word !== "");
+}
+
+export function startREPL() {
+  const readInterface = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Pokedex > ',
+  });
+
+  readInterface.prompt();
+  readInterface.on('line', (input: string) => {
+    const cleaned: string[] = cleanInput(input);
+    if (!cleaned) {
+      readInterface.prompt();
+      return;
+    }
+    console.log(`Your command was: ${cleaned[0]}`);
+    readInterface.prompt();
+  });
 }
