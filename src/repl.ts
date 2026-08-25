fe6f10048db77740e65f1e74a440d8a0d9ab4769
SpencerMcFadden <mcfadden.spencer@protonmail.com>
2026-08-25 13:29:19 -0400

    adds state object to handle command registry and give commands access to readline interface.

--- src/repl.ts
+++ src/repl.ts
@@ -1,7 +1,5 @@
-import { createInterface } from 'node:readline';
-import { stdin, stdout } from 'node:process';
-
 import { getCommands } from "./commands/get_commands.js";
+import { State } from "./state.js";
 
 
 export function cleanInput(input:string): string[]{
@@ -12,39 +10,32 @@ export function cleanInput(input:string): string[]{
     .filter((word) => word !== "");
 }
 
-export function startREPL() {
-  const readInterface = createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    prompt: 'Pokedex > ',
-  });
-
-  readInterface.prompt();
-  readInterface.on('line', async (input: string) => {
+export function startREPL(state: State) {
+  state.rl.prompt();
+  state.rl.on('line', async (input: string) => {
     const cleaned: string[] = cleanInput(input);
     if (!cleaned) {
-      readInterface.prompt();
+      state.rl.prompt();
       return;
     }
 
     const commandName = cleaned[0];
 
-    const commands = getCommands();
-    const userCommand = commands[commandName];
+    const userCommand = state.commands[commandName];
     if (!userCommand) {
       console.log(
         `Unknown command: "${commandName}". Type "help" for a list of commands.`,
       );
-      readInterface.prompt();
+      state.rl.prompt();
       return
     }
 
     try {
-      userCommand.callback(commands);
+      userCommand.callback(state);
     } catch (e) {
       console.log(e);
     }
 
-    readInterface.prompt();
+    state.rl.prompt();
   });
 }
---
+++ src/state.ts
@@ -0,0 +1,25 @@
+import { createInterface, type Interface } from "readline";
+
+import { getCommands } from "./commands/get_commands.js";
+
+export type CLICommand = {
+  name: string;
+  description: string;
+  callback: (state: State) => void;
+}
+
+export type State = {
+  rl: Interface;
+  commands: Record<string, CLICommand>;
+}
+
+export function initState(): State {
+  return {
+    rl: createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      prompt: 'Pokedex > ',
+    }),
+    commands: getCommands(),
+  };
+}
